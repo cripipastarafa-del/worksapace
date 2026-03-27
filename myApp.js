bd31ad04269f7fabb921c49dd203006c60827a26
cripipastarafa-del <cripipastarafa@gmail.com>
2026-03-27 18:41:47 +0100

    fix: update xssFilter usage and add noSniff middleware for improved security

--- myApp.js
+++ myApp.js
@@ -51,7 +51,8 @@ const api = require('./server.js');
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.disable('strict-transport-security');
-app.use(helmet.xssFilter({}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff())
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
